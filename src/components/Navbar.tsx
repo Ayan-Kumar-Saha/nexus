d27103d999b9ea00a90469f7cd47979d6fba226f
Ayan <ayankumarsaha96@gmail.com>
2025-04-05 20:36:54 +0530

    feat: add navigation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,26 @@
 import { FunctionComponent } from "react";
-import { Link } from "react-router";
+import { Link, useLocation } from "react-router";
 import { NAV_LINKS } from "../models/navlinks";
 import { INavLink } from "../interfaces/navlink";
 
 const Navbar: FunctionComponent = () => {
+
+    const location = useLocation();
+
     return (
-        <header className="sticky top-0 backdrop-blur-lg bg-background/90 border-b border-border z-50">
+        <header className="sticky top-0 backdrop-blur-lg bg-page-background border-b border-border-default z-50">
             <div className="nx-container py-3">
                 <nav className="flex justify-between items-center">
-                    <Link to='/' className="font-code text-lg md:text-xl font-bold">
-                        <span className="text-primary">&gt;</span> ayan<span className="text-primary">_</span>saha
+                    <Link to='/' className="font-code text-xl md:text-2xl font-bold hover:text-accent-text transition-colors">
+                        <span className="text-accent-text">&gt;</span> ayan<span className="text-accent-text">_</span>kumar<span className="text-accent-text">_</span>saha
                     </Link>
                     <ul className="flex items-center space-x-6">
                         {
                             NAV_LINKS.map((navlink: INavLink) => {
                                 return (
                                     <li key={navlink.id}>
-                                        <Link to={navlink.path} className="font-medium">{navlink.name}</Link>
+                                        <Link to={navlink.path}
+                                            className={`font-medium hover:text-accent-text transition-colors ${navlink.path === location.pathname ? 'text-accent-text' : 'text-primary-text'}`}>{navlink.name}</Link>
                                     </li>
                                 )
                             })
