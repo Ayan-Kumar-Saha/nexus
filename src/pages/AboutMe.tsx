a8f1c490d45ed803b0797484b8a8a2d2e3a3e66d
Ayan <ayankumarsaha96@gmail.com>
2025-06-27 19:32:26 +0530

    chore: update about me content

--- src/pages/AboutMe.tsx
+++ src/pages/AboutMe.tsx
@@ -44,16 +44,16 @@ const AboutMe = () => {
                 <div className="flex-1/2">
                     <div className="my-3 text-sm md:text-base">
                         <p className="text-justify">
-                            Hey, I’m Ayan Kumar Saha — a full-stack developer who’s all about building things that work and feel right. Clean code, clear intent, and a good user experience — that’s my happy place.
+                            Hey, I’m Ayan Kumar Saha, a full-stack engineer who’s all about building things that work and feel right. Clean code, clear intent, and a good user experience - that’s my happy place.
                         </p>
                         <p className="mt-3 text-justify">
-                            I’ve been doing this for over six years now. From small websites to enterprise-level systems, I’ve worn a lot of hats — but it’s never been just about ticking boxes. I care about the craft. I enjoy solving problems that matter and making digital spaces better.
+                            I’ve been doing this for over six years now. From small websites to enterprise-level systems, I’ve worn a lot of hats. But it’s never been just about ticking boxes. I care about the craft. I enjoy solving problems that matter and making digital spaces better.
                         </p>
                         <p className="mt-3 text-justify">
                             Outside of work, I’m a finance enthusiast, an unapologetic cricket addict, and someone who genuinely enjoys the occasional escape to the bed, for good sleep.
                         </p>
                         <p className="mt-3 text-justify">
-                            I’m currently based in {CONTACT_METHODS.find(method => method.name === 'Location')?.value} — open to remote roles and freelance gigs that are worth the time and energy. If you’ve got something interesting, let’s chat.
+                            I’m currently based in {CONTACT_METHODS.find(method => method.name === 'Location')?.value} - open to remote roles and freelance gigs that are worth the time and energy. If you’ve got something interesting, let’s chat.
                         </p>
                     </div>
                     <div className="flex mt-10 gap-3">
